316be697f3a513195ce1c2467d0615052b76e238
Credo Duarte <credo_duarte@brown.edu>
2020-07-12 19:54:13 -0400

    setup

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 class="heading">This will be a header</h1>
+    <p>This will be some sample text: Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate qui eligendi provident et saepe repellendus aut! Modi magnam eaque debitis odit recusandae dolores porro. Asperiores incidunt culpa optio quasi nisi!</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
+
